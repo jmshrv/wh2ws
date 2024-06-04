85a7b9b2202c3165a8546a8c736ac58176ffa271
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-06-04 14:32:58 +0100

    Begin work tracking webhooks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::{collections::HashMap, env, path::PathBuf};
+
+use rocket::Data;
+use tokio_stream::{self as stream, StreamExt};
+
+#[macro_use]
+extern crate rocket;
+
+type WebhookMap = HashMap<String, stream::Pending<String>>;
+
+#[post("/webhook/<path..>", data = "<payload>")]
+fn webhook(path: PathBuf, payload: Data<'_>) {}
+
+#[get("/websocket/<path..>")]
+fn websocket(path: PathBuf) {}
+
+#[launch]
+fn rocket() -> _ {
+    let webhooks = env::var("WEBHOOKS")
+        .expect("No WEBHOOKS environment variable set!")
+        .split(',')
+        .map(|path| (path.to_string(), stream::pending::<String>()))
+        .collect::<HashMap<_, _>>();
+
+    rocket::build()
+        .mount("/", routes![webhook])
+        .manage(webhooks)
 }
