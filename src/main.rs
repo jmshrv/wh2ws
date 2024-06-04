b6306b5758d2c197e2c7166c625ad800fc996091
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-06-04 15:57:29 +0100

    Use broadcast receivers (fixes multiple websockets)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::{collections::HashMap, env, path::PathBuf};
 
-use flume::Receiver;
-use flume::Sender;
 use rocket::futures::SinkExt;
 use rocket::State;
 use rocket_ws::Channel;
 use rocket_ws::WebSocket;
+use tokio::sync::broadcast;
+use tokio::sync::broadcast::Receiver;
+use tokio::sync::broadcast::Sender;
 
 #[macro_use]
 extern crate rocket;
@@ -13,7 +14,7 @@ extern crate rocket;
 type WebhookMap = HashMap<String, (Sender<String>, Receiver<String>)>;
 
 #[post("/webhook/<path..>", data = "<payload>")]
-async fn webhook(path: PathBuf, payload: String, webhooks: &State<WebhookMap>) {
+fn webhook(path: PathBuf, payload: String, webhooks: &State<WebhookMap>) {
     let path = path.to_string_lossy().to_string();
     let webhook = webhooks.get(&path).expect("Invalid webhook path!");
 
@@ -26,11 +27,11 @@ fn websocket(path: PathBuf, ws: WebSocket, webhooks: &State<WebhookMap>) -> Chan
     let path = path.to_string_lossy().to_string();
     let webhook = webhooks.get(&path).expect("Invalid webhook path!");
 
-    let receiver = webhook.1.clone();
+    let mut receiver = webhook.0.subscribe();
 
     ws.channel(move |mut stream| {
         Box::pin(async move {
-            while let Ok(message) = receiver.recv_async().await {
+            while let Ok(message) = receiver.recv().await {
                 stream.send(rocket_ws::Message::Text(message)).await?;
             }
 
@@ -44,7 +45,7 @@ fn rocket() -> _ {
     let webhooks = env::var("WEBHOOKS")
         .expect("No WEBHOOKS environment variable set!")
         .split(',')
-        .map(|path| (path.to_string(), flume::unbounded::<String>()))
+        .map(|path| (path.to_string(), broadcast::channel::<String>(10000)))
         .collect::<HashMap<_, _>>();
 
     rocket::build()
